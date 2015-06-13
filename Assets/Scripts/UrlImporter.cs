efe8eeb4c90fc490e3d1f00d7b4be96e77ae0b68
Paúl Fernández <b02341@ecci.ucr.ac.cr>
2015-06-13 11:45:07 -0600

    cargador de imagenes

---
+++ Assets/Scripts/UrlImporter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UrlImporter : MonoBehaviour {
+    bool terminado;
+    
+
+    public void urlToSprite(string path, Image imagen) {
+        Debug.Log(path);
+        StartCoroutine(cargarImagen(path, (value) => {
+                                                          if (value) { 
+                                                              Debug.Log("entro"); 
+                                                              imagen.sprite = value; 
+                                                          }
+                                                     }));    
+    }
+
+    IEnumerator cargarImagen(string path,System.Action<Sprite> newSprite){
+        WWW www = new WWW(path);
+        yield return www;
+        if (www.error == null)
+        {
+            Texture2D texture = new Texture2D(1024, 1024, TextureFormat.RGB565, false);
+            www.LoadImageIntoTexture(texture);
+            newSprite(Sprite.Create(texture, new Rect(0f, 0f, texture.width, texture.height), new Vector2(0.5f, 0.5f)));
+            Debug.Log("imagen cargada con éxito");
+            
+        }
+        else {
+            this.terminado = true;
+            Debug.Log("error cargando la imagen ");
+            newSprite(null);
+        }
+        
+    }
+}
