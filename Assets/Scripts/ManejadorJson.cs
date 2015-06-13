2fce7ddcaf03258e83bddc914ae2c278b5b18f9c
NamelessGuy <nitemare.andree@gmail.com>
2015-06-13 17:36:09 -0600

    asdsadasd
    asdsad

--- Assets/Scripts/ManejadorJson.cs
+++ Assets/Scripts/ManejadorJson.cs
@@ -6,17 +6,38 @@ public class ManejadorJson : MonoBehaviour {
 	
 	public void crearDepartamento(string nombreInterno, string nombre,string imagen){
 		var N = JSONNode.Parse(j);
+		
 		N[nombreInterno]["nombre"]=nombre;
 		N[nombreInterno]["imagen"]=imagen;
 		j=N.ToString();
 	}
 	
-	public void crearJuego(string departamento, string nombre, string fondo, string obstaculos, string imgPath){
+	public void crearJuego(string departamento, string nombre, string fondo){
 		var N = JSONNode.Parse(j);
 		N[departamento]["juego"]["nombre"]=nombre;
 		N[departamento]["juego"]["fondo"]=fondo;
-		N[departamento]["juego"]["obstaculos"]=obstaculos;
-		N[departamento]["juego"]["imgPath"]=imgPath;
+		j=N.ToString();
+	}
+	
+		public void annadirObstaculo(string departamento, string obstaculos){
+		var N = JSONNode.Parse(j);
+		int valor;
+		valor =N[departamento]["juego"]["numObstaculos"].AsInt;
+		N[departamento]["juego"]["obstaculos"][valor]=obstaculos;
+		++valor;
+		N[departamento]["juego"]["numObstaculos"]= ""+ valor;
+		
+		j=N.ToString();
+	}
+	
+		public void annadirAccesorio(string departamento, string accesorios){
+		var N = JSONNode.Parse(j);
+		int valor;
+		valor =N[departamento]["juego"]["numAccesorios"].AsInt;
+		N[departamento]["juego"]["accesorios"][valor]=accesorios;
+		++valor;
+		N[departamento]["juego"]["numAccesorios"]=""+ valor;
+		
 		j=N.ToString();
 	}
 	
@@ -28,11 +49,17 @@ public class ManejadorJson : MonoBehaviour {
 	
 	public void crearProducto(string departamento, string categoria, string nombre,string imagen, string url){
 		var N = JSONNode.Parse(j);
+		int valor;
+		valor =N[departamento]["categoria"][categoria]["numProductos"].AsInt;
+		++valor;
+		N[departamento]["categoria"][categoria]["numProductos"]=""+ valor;
 		N[departamento]["categoria"][categoria][nombre]["nombre"]=nombre;
 		N[departamento]["categoria"][categoria][nombre]["imagen"]=imagen;
 		N[departamento]["categoria"][categoria][nombre]["url"]=url;
 		j=N.ToString();
 	}
+	
+
 
 	public string obtenerJSON(){
 		return j;
---
+++ Assets/Scripts/importadorJson.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class importadorJson : MonoBehaviour {
+	
+	ManejadorJson k;
+	public GameObject antes;
+	public string apartamento;
+	string[] accesorios;
+	string[] obstaculos;
+	Sprite[] acc1;
+	Sprite[] obs1;
+	string json;
+	
+	void Start(){
+		//k = this.GetComponent<ManejadorJson>();
+		//testLlenarJSON();
+	}
+	/*
+	void testLlenarJSON(){
+		
+		
+		k.crearDepartamento("Gatadas","gato1","gato1.jpg");
+		k.crearJuego("Gatadas","Pollitos en fuga", "Huyendo.jpg");
+		k.annadirObstaculo("Gatadas","pidras");
+		k.annadirObstaculo("Gatadas","tortugas");
+		k.annadirObstaculo("Gatadas","porte y la vara");
+		k.annadirAccesorio("Gatadas","gotas");
+		k.annadirAccesorio("Gatadas","gatos");
+		k.annadirAccesorio("Gatadas","jardines");
+		k.annadirAccesorio("Gatadas","tenedores");
+		k.annadirAccesorio("Gatadas","cucharas");
+		//k.printJSON();
+		
+		json=k.obtenerJSON();
+		apartamento = "Gatadas";
+		separarJson();
+	}*/
+	
+	void meterJson(string jsonEntrante){
+		json=jsonEntrante;
+		separarJson();
+	}
+	
+	void separarJson(){
+		string fondo;
+		//Debug.Log(json);
+		
+		var N = JSONNode.Parse(json);
+		Debug.Log(N.ToString(""));
+		int x1;
+		int x2;
+		x1 =N[apartamento]["juego"]["numAccesorios"].AsInt;
+		x2 =N[apartamento]["juego"]["numObstaculos"].AsInt;
+		
+		acc1= new Sprite[x1];
+		obs1= new Sprite[x2];
+		Sprite fondoDibujo;
+		
+		accesorios = new string[x1];
+		obstaculos = new string[x2];
+		
+		Debug.Log("x1 " + x1);
+		Debug.Log("Ok mae, esta es la vara: obstaculos[x]");
+		for (int x=0;x<x2;++x){
+			obstaculos[x]=N[apartamento]["juego"]["obstaculos"][x];
+			Debug.Log("obstaculos[x] "+obstaculos[x]);
+			obs1[x]=(Sprite) Resources.Load(obstaculos[x]);
+		}
+		for (int x=0;x<x1;++x){
+			accesorios[x]=N[apartamento]["juego"]["Accesorios"][x];
+			Debug.Log("accesorios[x] "+accesorios[x]);
+			acc1[x]=(Sprite) Resources.Load(accesorios[x]);
+		}
+		fondo=N[apartamento]["juego"]["fondo"];
+		fondoDibujo=(Sprite) Resources.Load(fondo);
+		//test(antes,acc1,obs1,fondoDibujo);
+		
+	}
+	
+	void setJSON(string temp){
+		json = temp;
+	}
+	
+	void setApartamento(string apartamentoNueva){
+		apartamento=apartamentoNueva;
+	}
+	
+	string[] getProductos(string cat){
+		var N = JSONNode.Parse(json);
+		int valor;
+		valor = N[apartamento]["categoria"][cat]["numProductos"].AsInt;
+		string[] elems;
+		elems= new string[valor];
+		for(int x=0;x<valor;++x){
+			elems[x]=N[apartamento]["categoria"][cat][x];
+		}
+		return elems;
+	}
+	
+
+	//public void test(GameObject anterior, Sprite[] player, Sprite[] enemies,Sprite background) {}
+}
