a88bb8cf4ad09c137f9d319614984c393c88447e
Malavasi <f.adrian59@yahoo.com>
2015-06-13 15:05:21 -0600

    Juego

---
+++ Assets/Scripts/Juegos/EndlessRunner 1/PlayerBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+
+    public GameObject player = null;
+    
+
+    void OnMouseDrag()
+    {
+        if(player != null)
+        {
+            Vector2 curScreenPoint = new Vector2(player.transform.localPosition.x, Input.mousePosition.y);
+            Vector2 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+            curPosition.x = player.transform.localPosition.x;
+            player.transform.localPosition = curPosition;
+        }
+    }
+}
