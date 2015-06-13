d54e928e5da101f93779e908136db8f8c00a122d
Paúl Fernández <b02341@ecci.ucr.ac.cr>
2015-06-13 17:27:23 -0600

    carousel

---
+++ Assets/Scripts/Carrousel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Carrousel : MonoBehaviour {
+
+    void cargarCarrousel(string sprites)
+    {
+        Sprite[] fondos = Resources.LoadAll<Sprite>(sprites);
+        Debug.Log(fondos.Length);
+        float ancho = Resources.Load<GameObject>("Prefabs/ImagenEnBlanco").GetComponent<RectTransform>().rect.width + 10f;
+        Debug.Log(ancho);
+        GameObject contenedor = gameObject.transform.GetChild(0).FindChild("Contenido").gameObject;
+        contenedor.GetComponent<RectTransform>().sizeDelta = new Vector2(ancho * fondos.Length, 120f);
+        for (int i = 0; i < fondos.Length; i++)
+        {
+            GameObject elementoDelCarousel = Instantiate(Resources.Load<GameObject>("Prefabs/ImagenEnBlanco"));
+            elementoDelCarousel.GetComponent<Image>().sprite = fondos[i];
+            elementoDelCarousel.transform.SetParent(gameObject.transform.GetChild(0).FindChild("Contenido"));
+            elementoDelCarousel.transform.localPosition = new Vector3(ancho * i, 0f, 0f);
+        }
+    }
+}
--- Assets/Scripts/InsertarDepartamentos.cs
+++ Assets/Scripts/InsertarDepartamentos.cs
@@ -8,22 +8,10 @@ public class InsertarDepartamentos : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        this.cargarCarrousel();
+        
 	}
 
-    void cargarCarrousel() {
-        Sprite [] fondos = Resources.LoadAll<Sprite>("Textures");
-        Debug.Log(fondos.Length);
-        for (int i = 0; i < fondos.Length; ++i)
-        {
-            GameObject elementoDelCarousel = Instantiate(Resources.Load<GameObject>("Prefabs/ImagenEnBlanco"));
-            Debug.Log(elementoDelCarousel.name);
-            elementoDelCarousel.GetComponent<Image>().sprite = fondos[i];
-            elementoDelCarousel.transform.SetParent(carrousel.transform.FindChild("Contenido"));
-            elementoDelCarousel.transform.localPosition = new Vector3((elementoDelCarousel.GetComponent<RectTransform>().rect.width + 10f) * i, 0f, 0f);
-        
-        }
-    }
+    
 	// Update is called once per frame
 	void Update () {
 	
